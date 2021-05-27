990a120af53f0c3414a0703db5aafb1ff86fc889
janarthan subburaj <janathanreddy@gmail.com>
2021-05-28 02:16:50 +0530

    first commit

---
+++ FoodDeliveryAppNGFO/Cells.swift
@@ -0,0 +1,8 @@
+//
+//  Cells.swift
+//  FoodDeliveryAppNGFO
+//
+//  Created by Admin Macappstudio on 27/05/21.
+//
+
+import Foundation
---
+++ FoodDeliveryAppNGFO/CollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CollectionViewCell.swift
+//  FoodDeliveryAppNGFO
+//
+//  Created by Admin Macappstudio on 27/05/21.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
